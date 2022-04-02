66926e40fffd511ce6b9003f68e167455fcfe625
Samsul Hadi <samsulhadikediri@gmail.com>
2022-04-02 14:24:45 +0700

    feat: refresh

--- usom/urls.py
+++ usom/urls.py
@@ -10,6 +10,6 @@ app_name = "usom_app"
 
 urlpatterns = [
     path("account/login/", TokenObtainPairView.as_view(), name="token_login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("account/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("account/logout/", LogoutView.as_view(), name="auth_logout"),
 ]
