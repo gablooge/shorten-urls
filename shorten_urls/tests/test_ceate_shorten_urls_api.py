87befd81964347911133c0965b8683b908c33a7a
Samsul Hadi <samsulhadikediri@gmail.com>
2022-04-03 09:41:45 +0700

    test: add invalid payload test

--- shorten_urls/tests/test_ceate_shorten_urls_api.py
+++ shorten_urls/tests/test_ceate_shorten_urls_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django.urls import reverse
 
 from usom.tests.conftest import *  # noqa: F401, F403
@@ -39,3 +41,29 @@ def test_create_shorten_url_super_user_empty_payload(
     )
     assert resp.json()["url"][0] == "This field is required."
     assert resp.status_code == 400
+
+
+@pytest.mark.parametrize(
+    "client, url, error, get_superuser_token, django_db_setup",
+    [
+        ("", None, "This field may not be null.", "", ""),
+        ("", "", "This field may not be blank.", "", ""),
+        ("", "wrong", "Enter a valid URL.", "", ""),
+    ],
+    indirect=["client", "get_superuser_token", "django_db_setup"],
+)
+def test_create_shorten_url_super_user_invalid_payload(
+    client, url, error, get_superuser_token, django_db_setup
+):
+    uri = reverse("shorten_urls_api:ShortenURL-list")
+    client.defaults["HTTP_AUTHORIZATION"] = "Bearer {}".format(
+        get_superuser_token["access"]
+    )
+
+    resp = client.post(
+        uri,
+        data={"url": url},
+        content_type="application/json",
+    )
+    assert resp.status_code == 400
+    assert resp.json()["url"][0] == error
